81d6f9bd203b5dc4120d91acefe83deb56da5032
sc0Vu <alk03073135@gmail.com>
2019-03-11 23:30:42 +0800

    Add chinese wordlist

--- test/unit/WalletTest.php
+++ test/unit/WalletTest.php
@@ -5,6 +5,7 @@ namespace Test\Unit;
 use Test\TestCase;
 use BitWasp\BitcoinLib\BIP39\BIP39EnglishWordList;
 use Web3p\EthereumWallet\Wallet;
+use Web3p\EthereumWallet\Wordlist\BIP39ChineseTraditionalWordList;
 
 class WalletTest extends TestCase
 {
@@ -17,6 +18,24 @@ class WalletTest extends TestCase
     {
         $wordlist = new BIP39EnglishWordList;
         $wallet = new Wallet($wordlist);
+        $wallet = $wallet->generate();
+        $this->assertEquals(64, strlen($wallet->privateKey));
+        $this->assertEquals(42, strlen($wallet->address));
+        $mnemonic = preg_split("/\s/", $wallet->mnemonic);
+        $this->assertEquals(24, count($mnemonic));
+    }
+
+    /**
+     * testSetWordlist
+     * 
+     * @return void
+     */
+    public function testSetWordlist()
+    {
+        $wordlist = new BIP39EnglishWordList;
+        $wallet = new Wallet($wordlist);
+        $zh_TW_wordlist = new BIP39ChineseTraditionalWordList;
+        $wallet->wordlist = $zh_TW_wordlist;
         $wallet = $wallet->generate();
         $this->assertEquals(64, strlen($wallet->privateKey));
         $this->assertEquals(42, strlen($wallet->address));
